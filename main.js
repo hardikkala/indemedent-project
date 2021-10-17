e78c30d09557e42f5ede66f6c32fe48896ca183a
hardikkala <84086962+hardikkala@users.noreply.github.com>
2021-10-17 09:12:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function rifle1()
+{
+    window.alert("The AK-47 is a Russian assault rifle first used in 1949. It and an updated version called the AKM were used by the Soviet Union's military (which was called the Soviet Army). It was later replaced by the AK-74.
+
+    The AK-47 was designed in 1947 by Mikhail Kalashnikov.
+    
+    The AK-47 quickly became famous and spread all around the world because it was simple to fire, clean and maintain, and also because of its reliability, meaning that it can be fired for a long time without jamming. The AK-47 and its successors continue to be used by many of the world's armies. Many terrorist and insurgent groups also use the AK-47. It is a cheap, reliable, and easy-to-use weapon. The AK-47 was also available with a folding stock, the AKS-47, and a shortened version with the AKS74 folding stock, the AKMSU (used by armoured vehicle crews), although this was soon replaced by the AKS74U, which fires the 5.45 cartridge of the AK-74. There was also a light machine gun variant with a longer barrel and different shaped stock called the RPK.
+    
+    The Russian military liked the AK's design so much that it was even used to design other types of weapons as well, including the Dragunov sniper rifle and the Saiga-12 semi-automatic shotgun.
+    
+    The AK-47 uses gas-operated reloading. When the bullet is moved down the barrel, a little bit of the gas behind the bullet is made to go up a small tube that pushes away the bolt. The shooter does not have to reload by hand for every shot - the gun reloads by itself. When you pull the trigger, the bullet in the chamber fires. You then release and then pull the trigger again to fire another round. When used this way, it is called a semi-automatic firearm. A few AK-47's are made to be used only this way but most are fully automatic firearms.");
+}
